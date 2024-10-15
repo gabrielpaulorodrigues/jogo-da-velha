a49d571045c908a771d4183b07b070297878b7d2
Gabriel Paulo Rodrigues <gabrielpaulogpsc@gmail.com>
2024-10-14 21:33:56 -0300

    projeto em andamento

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
+function Square({ value }){
 
-    </div>
-  );
+  function handleClick(){
+    setValue('X');
+  }
+
+  return ( <button 
+  className="square"
+  onClick={handleClick}
+  >
+  {value}
+  </button>
+  )
 }
 
-export default App;
+export default function Board(){
+
+  const[squares, setSquares] = useState(Array(9).fill(null))
+
+  return(
+  <>
+  <div className="board-row">
+    <Square value={squares[0]}/>
+    <Square value={squares[1]}/>
+    <Square value={squares[2]}/>
+  </div>
+  <div className="board-row">
+    <Square value={squares[3]}/>
+    <Square value={squares[4]}/>
+    <Square value={squares[5]}/>
+  </div>
+  <div className="board-row">
+    <Square value={squares[6]}/>
+    <Square value={squares[7]}/>
+    <Square value={squares[9]}/>
+  </div>
+  </>
+  )
+}
